3f0b9b3a36dcb94bfcc7d1502e9b7ed2a3e7862b
Pedro Ferreira <pedro.ferreirasilva777@gmail.com>
2024-07-30 13:00:01 -0300

    Primeiro commit.

---
+++ buscar-cursos.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client as GuzzleHttp;
+use Symfony\Component\DomCrawler\Crawler;
+use phFerreira\BuscadorDeCursos\Buscador;
+
+$client = new GuzzleHttp([
+    'base_uri' => 'https://www.alura.com.br/cursos-online-programacao/php',
+    'verify' => false
+]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
